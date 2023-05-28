dd48e0680702bbcddd8a7cf8a6e26bb1b6012203
stefanodaroit <stefano.daroit@gmail.com>
2023-05-28 17:47:55 +0200

    Moved distance kernels

--- src/kmeans.cu
+++ src/kmeans.cu
@@ -14,78 +14,11 @@
 
 #include "kernels/kernels.cuh"
 
-#define SHFL_MASK 0xFFFFFFFF
-
 using namespace std;
 
 random_device rd;
 seed_seq seed{0}; // FIXME use rd()
 mt19937 rng(seed);
-
-// This function is not decalred in utils.cu because of compilation problems with __host__ __device__
-__host__ __device__ unsigned int next_pow_2(unsigned int x) {
-  --x;
-  x |= x >> 1;
-  x |= x >> 2;
-  x |= x >> 4;
-  x |= x >> 8;
-  x |= x >> 16;
-  return ++x;
-}
-
-/*  DISTANCES KERNELS  */
-__global__ void compute_distances_one_point_per_warp(DATA_TYPE* distances, DATA_TYPE* centroids, DATA_TYPE* points) {
-  const uint64_t point_offset = blockIdx.x * blockDim.x + threadIdx.x;
-  const uint64_t center_offset = blockIdx.y * blockDim.x + threadIdx.x;
-  DATA_TYPE dist = points[point_offset] - centroids[center_offset];
-  dist *= dist;
-  
-  for (int i = next_pow_2(blockDim.x); i > 0; i /= 2)
-    dist += __shfl_down_sync(SHFL_MASK, dist, i);
-
-  if (threadIdx.x == 0) {
-    distances[(blockIdx.x * gridDim.y) + blockIdx.y] = dist;
-  }
-}
-
-__global__ void compute_distances_shmem(DATA_TYPE* distances, DATA_TYPE* centroids, DATA_TYPE* points, const uint32_t points_per_warp, const uint32_t d) {
-  const uint64_t point_i = (blockIdx.x * points_per_warp) + (threadIdx.x / d);
-  const uint64_t center_i = blockIdx.y;
-  const uint32_t d_i = threadIdx.x % d;
-  const uint64_t dists_i = (center_i * blockDim.y * d) + ((point_i % points_per_warp) * d) + d_i;
-
-  extern __shared__ DATA_TYPE dists[];
-
-  if (threadIdx.x < points_per_warp * d) {
-    DATA_TYPE dist = fabs(points[point_i * d + d_i] - centroids[center_i * d + d_i]);
-    dists[dists_i] = dist * dist;
-    __syncthreads();
-    if (d_i == 0) {
-      for (int i = 1; i < d; i++) {
-        dists[dists_i] += dists[dists_i + i];
-      }
-      distances[(point_i * center_i) + point_i] = dists[dists_i];
-    }
-  }
-}
-
-__global__ void compute_distances_shfl(DATA_TYPE* distances, DATA_TYPE* centroids, DATA_TYPE* points, const uint32_t points_n, const uint32_t points_per_warp, const uint32_t d, const uint32_t d_closest_2_pow) {
-  const uint64_t point_i = (blockIdx.x * points_per_warp) + (threadIdx.x / d_closest_2_pow);
-  const uint64_t center_i = blockIdx.y;
-  const uint32_t d_i = threadIdx.x % d_closest_2_pow;
-
-  if (point_i < points_n && d_i < d) {
-    DATA_TYPE dist = fabs(points[point_i * d + d_i] - centroids[center_i * d + d_i]);
-    dist *= dist;
-    for (int i = d_closest_2_pow / 2; i > 0; i /= 2) {
-      dist += __shfl_down_sync(SHFL_MASK, dist, i);
-      // if (point_i == 3) printf("%d  p: %lu c: %lu d: %u v: %.3f\n", i, point_i, center_i, d_i, dist);
-    }
-    if (d_i == 0) {
-      distances[(point_i * gridDim.y) + center_i] = dist;
-    }
-  }
-}
 
 /* Kmeans class */
 void Kmeans::init_centroids (Point<DATA_TYPE>** points) {
@@ -222,7 +155,7 @@ uint64_t Kmeans::run (uint64_t maxiter) {
     #elif COMPUTE_DISTANCES_KERNEL == 2     
       compute_distances_shfl<<<dist_grid_dim, dist_block_dim>>>(d_distances, d_centroids, d_points, n, dist_max_points_per_warp, d, next_pow_2(d));
     #else
-      compute_distances_one_point_per_warp<<<dist_grid_dim, dist_block_dim>>>(d_distances, d_centroids, d_points);
+      compute_distances_one_point_per_warp<<<dist_grid_dim, dist_block_dim>>>(d_distances, d_centroids, d_points, next_pow_2(dist_block_dim.x));
     #endif
     CHECK_CUDA_ERROR(cudaDeviceSynchronize());  
     #if PERFORMANCES_KERNEL_DISTANCES
